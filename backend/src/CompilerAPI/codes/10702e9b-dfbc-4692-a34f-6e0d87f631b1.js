15dbebf993d945abd484bd8be54ea5e986c39b78
Tushar <naintushar@hotmail.com>
2022-08-06 09:22:24 +0530

    initial upload

--- backend/src/CompilerAPI/codes/10702e9b-dfbc-4692-a34f-6e0d87f631b1.js
+++
@@ -1,5 +0,0 @@
-let str = "hello there was there now there";
-
-let newStr = str.replace(/there/g,c "Tushar");
-
-console.log(newStr);
--- backend/src/CompilerAPI/codes/5c1ba151-8f84-4e1e-aa80-3459f647bceb.js
+++
@@ -1,5 +0,0 @@
-let str = "hello there was there now there";
-
-let newStr = str.replace(/txhere/g,c "Tushar");
-
-console.log(newStr);
